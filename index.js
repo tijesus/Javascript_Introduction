c083334c96758ff133dcde156ed4e8e36bd337a9
tijesus <emmanueltijesu@gmail.com>
2022-06-20 21:40:45 +0100

    First commit of introduction

---
+++ index.js
@@ -0,0 +1,7 @@
+var myName = "Emmanuel Gabriel";
+    var myHeight = "1.65m";
+    var myCountry = "Nigeria";
+
+    document.getElementById("myName").innerHTML = "My Name is: " + myName;
+    document.getElementById("myHeight").innerHTML = "My Height is: " + myHeight;
+    document.getElementById("myCountry").innerHTML = "My Country is: " + myCountry;
